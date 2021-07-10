53d3bd6b53ebfdea9d042bbf9073c35d26ae177c
Phong Huynh <p7157568@gmail.com>
2021-07-10 11:39:22 +0700

    Add preview

---
+++ app/src/main/java/com/umbrella/likeanimbutton/widget/LikeAnimButton2.kt
@@ -0,0 +1,18 @@
+package com.umbrella.likeanimbutton.widget
+
+import android.content.Context
+import android.graphics.Canvas
+import android.util.AttributeSet
+import android.view.View
+import com.google.android.material.math.MathUtils
+
+class LikeAnimButton2 @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyle: Int = 0
+) : View(context, attrs, defStyle) {
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+    }
+}
